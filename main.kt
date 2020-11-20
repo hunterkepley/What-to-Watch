03d9b2f910e4caa56d740ae3d4cd7771203f774d
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-11-20 12:47:39 -0500

    Started CSV Parsing

---
+++ main.kt
@@ -0,0 +1,8 @@
+package main.whattowatch
+
+fun main() {
+    println("Hello World!")
+
+    var parser = MovieParser()
+    parser.parseMovies()
+}
---
+++ movieParser.kt
@@ -0,0 +1,7 @@
+package main.whattowatch
+
+class MovieParser {
+    fun parseMovies() {
+        println("\nparsing movies...")
+    }
+}
