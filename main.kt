69350bfad20536a5a2d8cc7dbf4b274b45359369
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-11-25 13:46:17 -0500

    Added breaks and number of movies parsed

--- main.kt
+++ main.kt
@@ -6,7 +6,11 @@ fun main() {
     var parser = MovieParser()
     var filterer = MovieFilter()
     parser.parseMovies() // Parses CSV into Kotlin
+    println("Press enter to continue...")
+    readLine()
     parser.printMovies()
+    println("Press enter to continue...")
+    readLine()
     var movies2016 = filterer.filterByYear(parser.movies, "1999")
     for (n in 1 until movies2016.size) {
         println(movies2016[n].title)
