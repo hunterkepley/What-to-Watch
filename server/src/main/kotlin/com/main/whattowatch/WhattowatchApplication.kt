33fa3ba7aff34a51d626713fb64d602bf5ad6943
Marwa Alqatari <marwaalqatari@gmail.com>
2020-11-25 15:21:55 -0500

    Creating Initial Server

---
+++ server/src/main/kotlin/com/main/whattowatch/WhattowatchApplication.kt
@@ -0,0 +1,11 @@
+package com.main.whattowatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhattowatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhattowatchApplication>(*args)
+}
