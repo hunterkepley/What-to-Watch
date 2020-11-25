11a6a57831d53f94860ec6fbc5c202878bb64c1f
Hunter Kepley <kepley.l.hunter@gmail.com>
2020-11-25 13:41:37 -0500

    Added filterByYear

--- main.kt
+++ main.kt
@@ -4,7 +4,11 @@ fun main() {
     println("Hello World!")
 
     var parser = MovieParser()
+    var filterer = MovieFilter()
     parser.parseMovies() // Parses CSV into Kotlin
-    parser.printMovieTitles()
     parser.printMovies()
+    var movies2016 = filterer.filterByYear(parser.movies, "1999")
+    for (n in 1 until movies2016.size) {
+        println(movies2016[n].title)
+    }
 }
